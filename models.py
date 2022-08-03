7e707bbf3fd5a3ebd59835129f2791f51a65a3ce
arakamboob <1pcpcpc1pc@gmail.com>
2022-08-03 13:03:23 +0300

    Folder record model (pre-working)

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, ForeignKey
+from uuid import uuid4
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -10,13 +11,25 @@ class PublicKeyRecord(Base):
     id = Column(String, primary_key=True)
     key_data = Column(String)
 
-    files = relationship("FileRecord", backref="owner")
+    folders = relationship("FolderRecord", backref="owner")
+
+
+class FolderRecord(Base):
+    __tablename__ = "folders"
+
+    folder_id = Column(String, primary_key=True, default=uuid4)
+    owner_id = Column(String, ForeignKey("public_keys.id"))
+    folder_name = Column(String, default="/")
+    parent_folder_id = Column(String,
+                              ForeignKey("folders.folder_id"),
+                              nullable=True)
 
 
 class FileRecord(Base):
     __tablename__ = "files"
 
+    file_id = Column(String, primary_key=True, default=uuid4)
     key_id = Column(String, ForeignKey("public_keys.id"))
-    directory = Column(String, default="/")
+    directory = Column(String, ForeignKey("folders.name"), default="/")
     filename = Column(String)
     link = Column(String, primary_key=True)
