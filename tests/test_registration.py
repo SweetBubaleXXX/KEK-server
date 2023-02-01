ce82bf54bb9505623fa23e6b1cac0e044043b644
arakamboob <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-02-01 14:34:34 +0300

    Test for key in db, test requirements

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -2,6 +2,7 @@ import unittest
 from base64 import b64encode
 
 from fastapi.testclient import TestClient
+from httpx import Response
 from fastapi import status
 from KEK.hybrid import PrivateKEK
 
@@ -60,15 +61,24 @@ class TestRegistration(unittest.TestCase):
 
     def test_token(self):
         key = PrivateKEK.generate()
-        response = self.client.post("/register", json=self._public_key_info(key))
-        token = response.json().get("token")
-        signed_token = b64encode(key.sign(token.encode("utf-8")))
-        response = self.client.post("/register", json=self._public_key_info(key),
-                                    headers={"Signed-Token": signed_token})
+        response = self._register_key(key)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_key_in_db(self):
+        key = PrivateKEK.generate()
+        self._register_key(key)
+        key_record = self.session.query(models.KeyRecord).filter_by(id=key.key_id.hex()).first()
+        self.assertEqual(key_record.public_key, key.public_key.serialize().decode("utf-8"))
 
     def _public_key_info(self, key: PrivateKEK) -> dict[str, str]:
         return {
             "key_id": key.key_id.hex(),
             "public_key": key.public_key.serialize().decode("utf-8")
         }
+
+    def _register_key(self, key: PrivateKEK) -> Response:
+        response = self.client.post("/register", json=self._public_key_info(key))
+        token = response.json().get("token")
+        signed_token = b64encode(key.sign(token.encode("utf-8")))
+        return self.client.post("/register", json=self._public_key_info(key),
+                                headers={"Signed-Token": signed_token})
