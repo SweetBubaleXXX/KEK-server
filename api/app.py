6f357a1fe74dc4c3f8a312dd7597c82b87510f13
arakamboob <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-02-01 12:04:30 +0300

    Separated app and main modules, __main__.py file

---
+++ api/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from .exceptions import exceptions, handlers
+from .routers import registration
+
+app = FastAPI()
+
+app.add_exception_handler(exceptions.RegistrationRequired,
+                          handlers.registration_required_handler)
+app.add_exception_handler(exceptions.AuthenticationRequired,
+                          handlers.authentication_required_handler)
+
+app.include_router(registration.router)
--- api/main.py
+++ api/main.py
@@ -1,22 +1,10 @@
-from fastapi import Depends, FastAPI
+import uvicorn
 
+from .app import app
 from .db import engine as db
-from .dependencies import verify_token
-from .exceptions import exceptions, handlers
-from .routers import registration
 
 db.Base.metadata.create_all(db.engine)
 
-app = FastAPI()
 
-app.add_exception_handler(exceptions.RegistrationRequired,
-                          handlers.registration_required_handler)
-app.add_exception_handler(exceptions.AuthenticationRequired,
-                          handlers.authentication_required_handler)
-
-app.include_router(registration.router)
-
-
-@app.post("/", dependencies=[Depends(verify_token)])
-def index():
-    ...
+def main():
+    uvicorn.run(app)
