cb8e16fd51827df38b79e099ae1561a204b0f3f6
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-02-07 10:32:46 +0300

    Fixed path formatters

--- api/utils/path_formatters.py
+++ api/utils/path_formatters.py
@@ -3,9 +3,13 @@ import os
 SEPARATOR = "/"
 
 
+def _normalize_path(path: str) -> str:
+    return os.path.normpath(path).strip(SEPARATOR)
+
+
 def split_into_components(path: str) -> list[str]:
-    return os.path.normpath(path).strip(f" / \t\r\n").split(SEPARATOR)
+    return _normalize_path(path).split(SEPARATOR)
 
 
-def split_and_format(path: str) -> tuple[str, str]:
-    return os.path.split(os.path.normpath(path).strip("/ \t\r\n"))
+def split_head_and_tail(path: str) -> tuple[str, str]:
+    return os.path.split(_normalize_path(path))
