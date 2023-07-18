7a92e321eabb02912b594393ea27a5c80c366577
SweetBubaleXXX <70811772+SweetBubaleXXX@users.noreply.github.com>
2023-07-18 10:09:39 +0300

    Database isolation level, driver requirements

--- api/config.py
+++ api/config.py
@@ -1,8 +1,17 @@
+from typing import Literal
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite+aiosqlite:///./db.sqlite3?check_same_thread=False"
+    DATABASE_ISOLATION_LEVEL: Literal[
+        "READ COMMITED",
+        "READ UNCOMMITED",
+        "REPEATABLE READ",
+        "SERIALIZABLE",
+        "AUTOCOMMIT",
+    ] = "SERIALIZABLE"
     USER_IS_ACTIVATED_DEFAULT: bool = False
     USER_STORAGE_SIZE_LIMIT: int = 0
     SESSION_STORAGE_MAX_SIZE: int = 1_000_000
--- api/db/engine.py
+++ api/db/engine.py
@@ -2,5 +2,8 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from .. import config
 
-engine = create_async_engine(config.settings.DATABASE_URL)
+engine = create_async_engine(
+    config.settings.DATABASE_URL,
+    isolation_level=config.settings.DATABASE_ISOLATION_LEVEL,
+)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
